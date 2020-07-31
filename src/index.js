75f70dbfd0594c557cd34ca6fdceff3c635c0a80
Anuraag Gupta <anuraagg@amazon.com>
2020-07-31 14:57:36 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { JEditor } from './package/index';
+import 'antd/dist/antd.css';
+
+// import {jeditor} from "../package/index.js";
+
+const mock = [
+  { name: '字符串', mock: '@string' },
+  { name: '自然数', mock: '@natural' },
+  { name: '浮点数', mock: '@float' },
+  { name: '字符', mock: '@character' },
+  { name: '布尔', mock: '@boolean' },
+  { name: 'url', mock: '@url' },
+  { name: '域名', mock: '@domain' },
+  { name: 'ip地址', mock: '@ip' },
+  { name: 'id', mock: '@id' },
+  { name: 'guid', mock: '@guid' },
+  { name: '当前时间', mock: '@now' },
+  { name: '时间戳', mock: '@timestamp' },
+];
+
+// const JEditor = jeditor({ mock: mock });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <br />
+    <h2>Example:</h2>
+    <hr />
+
+    <JEditor
+      showEditor={true}
+      isMock={false}
+      data={''}
+      onChange={(e) => {
+        console.log('changeValue', e);
+      }}
+      mock={mock}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
